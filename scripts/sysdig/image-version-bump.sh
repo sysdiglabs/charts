ebff12fd8c38037448536566340791a5a29e18a9
Angelo Puglisi <angelopuglisi86@gmail.com>
2021-07-14 17:39:07 +0200

    [sysdig] Version bump scripts (#186)
    * Script for chart version bump
    
    Introduced chart-version-bump.sh, incrementing patch version
    
    * [sysdig] Script for container image version bump
    
    Introduced image-version-bump.sh to help setting a new container image
    version for the following:
    - sysdig/agent
    - sysdig/node-image-analyzer
    - sysdig/host-analyzer

---
+++ scripts/sysdig/image-version-bump.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env sh
+
+set -e
+
+# not checking input format, just ensuring there's any
+if [[ -z "$@" ]]; then
+    >&2 echo "At least a param has to be passed."
+    >&2 echo "Example usage:"
+    >&2 echo "# cd charts/sysdig; ../../scripts/sysdig/image-version-bump.sh -v AGENT_VERSION=1.2.3 -v HOST_ANALYZER_VERSION=4.5.6"
+    exit 1
+fi
+
+awk $@ '
+BEGIN {
+    if (AGENT_VERSION)
+        version["sysdig/agent"] = AGENT_VERSION
+    if (IMAGE_ANALYZER_VERSION)
+        version["sysdig/node-image-analyzer"] = IMAGE_ANALYZER_VERSION
+    if (HOST_ANALYZER_VERSION)
+        version["sysdig/host-analyzer"] = HOST_ANALYZER_VERSION
+    if (BENCHMARK_RUNNER_VERSION)
+        version["sysdig/compliance-benchmark-runner"] = BENCHMARK_RUNNER_VERSION
+
+    if (length(version) == 0)
+        exit 1
+}
+
+{
+    if ($1 == "repository:") {
+        repository = $NF
+        print
+
+        # we assume tag to be right after
+        getline
+
+        if (repository in version)
+            sub(/:.*/, ": "version[repository])
+    }
+    print
+}
+' values.yaml > values.yaml.2
+
+mv values.yaml.2 values.yaml
