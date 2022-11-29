89936486412161c26979ad095886b0320036844d
Michele Palazzi <michele.palazzi@sysdig.com>
2022-11-29 09:44:03 +0100

    add quotes and fix potential unwanted behaviors
    Signed-off-by: Michele Palazzi <michele.palazzi@sysdig.com>

--- scripts/sysdig-deploy/update-sysdig-deploy.sh
+++ scripts/sysdig-deploy/update-sysdig-deploy.sh
@@ -16,7 +16,7 @@ patch=0
 
 # helper functions to preserve empty lines and avoid unnecessary changes
 bump_dep_preserve_blank() {
-    sed -i "$(yq '( .dependencies[] | select(.name == "'$1'") | .version) | line' "charts/$4/Chart.yaml")s/$2/$3/" "charts/$4/Chart.yaml"
+    sed -i "$(yq '( .dependencies[] | select(.name == "'"$1"'") | .version) | line' "charts/$4/Chart.yaml")s/$2/$3/" "charts/$4/Chart.yaml"
 }
 
 bump_main_preserve_blank() {
@@ -25,19 +25,19 @@ bump_main_preserve_blank() {
 
 check_update_needed () {
 
-    new_subchart_version=$(yq '.version' charts/$chart/Chart.yaml)
-    sysdig_subchart_version=$(yq '.dependencies[] | select(.name == "'$chart'") | .version ' charts/$sysdig_deploy_path/Chart.yaml | sed "s/~//")
+    new_subchart_version=$(yq '.version' charts/"$chart"/Chart.yaml)
+    sysdig_subchart_version=$(yq '.dependencies[] | select(.name == "'"$chart"'") | .version ' charts/$sysdig_deploy_path/Chart.yaml | sed "s/~//")
 
     IFS="." read -ra prev_ver <<< "$sysdig_subchart_version"
     IFS="." read -ra next_ver <<< "$new_subchart_version"
 
     echo "subchart version in ${chart}/Chart.yaml"
-    echo $new_subchart_version
-    echo $chart "subchart version in sysdig-deploy/Chart.yaml"
-    echo $sysdig_subchart_version
+    echo "$new_subchart_version"
+    echo "$chart" "subchart version in sysdig-deploy/Chart.yaml"
+    echo "$sysdig_subchart_version"
 
     for ((idx=0; idx<3; ++idx)); do
-        if [ ${next_ver[idx]} -gt ${prev_ver[idx]} ]
+        if [ "${next_ver[idx]}" -gt "${prev_ver[idx]}" ]
         then
             if [ $idx -eq 0 ]
             then
@@ -52,16 +52,16 @@ check_update_needed () {
         fi
     done
 
-    if [[ $minor > 0 || $major > 0 || $patch > 0  ]]; then
-        # update the subchart version in sysdig-deploy/Chart.yaml
-        bump_dep_preserve_blank $chart $sysdig_subchart_version $new_subchart_version $sysdig_deploy_path
+    # update the subchart version in sysdig-deploy/Chart.yaml
+    if [[ $minor -gt 0 || $major -gt 0 || $patch -gt 0  ]]; then
+        bump_dep_preserve_blank "$chart" "$sysdig_subchart_version" "$new_subchart_version" "$sysdig_deploy_path"
     fi
 }
 
 charts=( "$node_analyzer_chart_path" "$agent_chart_path" "$kspm_collector_chart_path" "$rapid_response_chart_path" "$admission_controller_chart_path" )
-for chart in ${charts[@]}
+for chart in "${charts[@]}"
 do
-    check_update_needed $chart
+    check_update_needed "$chart"
 done
 
 current_sysdig_deploy_version=$(yq '.version' charts/$sysdig_deploy_path/Chart.yaml)
@@ -70,24 +70,24 @@ echo -e "\nsysdig-deploy version : $current_sysdig_deploy_version"
 IFS='.'
 read -ra sysdig_deploy_version <<< "$current_sysdig_deploy_version"
 
-new_major=$((${sysdig_deploy_version[0]}))
-new_minor=$((${sysdig_deploy_version[1]}))
-new_patch=$((${sysdig_deploy_version[2]}))
+new_major=$((sysdig_deploy_version[0]))
+new_minor=$((sysdig_deploy_version[1]))
+new_patch=$((sysdig_deploy_version[2]))
 
 if [ $major -eq 1 ]
 then
-    new_major=$(($new_major + 1))
+    new_major=$((new_major + 1))
     new_minor=0
     new_patch=0
     echo "major version change needed"
 elif [ $minor -eq 1 ]
 then
-    new_minor=$(($new_minor + 1))
+    new_minor=$((new_minor + 1))
     new_patch=0
     echo "minor version change needed"
 elif [ $patch -eq 1 ]
 then
-    new_patch=$(($new_patch + 1))
+    new_patch=$((new_patch + 1))
     echo "patch version change needed"
 else
     echo "No modifications necessary"
@@ -97,4 +97,4 @@ fi
 IFS=$OLDIFS
 final_sysdig_deploy_version=$(echo -n "$new_major.$new_minor.$new_patch")
 echo "Final deploy version is to be : $final_sysdig_deploy_version"
-bump_main_preserve_blank $current_sysdig_deploy_version $final_sysdig_deploy_version $sysdig_deploy_path
+bump_main_preserve_blank "$current_sysdig_deploy_version" "$final_sysdig_deploy_version" "$sysdig_deploy_path"
